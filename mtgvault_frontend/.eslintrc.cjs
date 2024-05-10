49934498bb4862efda97452e715f132dc327d774
Emma Jensen <emmasarahlinnea@gmail.com>
2024-05-10 11:05:02 +0200

    Eslint, included vite in tsconfig json manually

--- mtgvault_frontend/.eslintrc.cjs
+++ mtgvault_frontend/.eslintrc.cjs
@@ -3,16 +3,19 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
+
+
