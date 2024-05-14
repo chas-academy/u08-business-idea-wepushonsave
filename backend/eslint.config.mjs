3e029d676b98b92a65a3e5ff33a346167d255ea8
LloydElery <dennisgooglar@gmail.com>
2024-05-14 22:35:10 +0200

    Backend server changes to be able to run lint, start the server and bypass certain lint errors like 'any'
    #Added an .env file that had gone missing
    #Deleted one of the config files for eslint
    #Added a jest config for testing
    #Removed 'type: module' from json
    #Added new command to scan all files via `npm run lint` instead of `npx eslint src/*`
    #Added a tsconfig.json file and changed settings to work with TS and our ESLint
    #Added bypass in db.ts

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,8 +1,20 @@
+import { ESLint } from "eslint";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
+
+  {
+    files: ["*.ts", "*.tsx"],
+    rules: {
+      "node/no-unsupported-features/es-syntax": [
+        "error",
+        { ignores: ["modules"] },
+      ],
+
+      "@typescript-eslint/no-explicit-any": ["off"],
+    },
+  },
 ];
