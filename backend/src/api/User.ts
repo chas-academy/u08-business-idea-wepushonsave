aa6334fa0a9aeb2ec3cc5e58769fe3b3f5173cfe
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-05-15 14:19:14 +0200

    Lint-fix

--- backend/src/api/User.ts
+++ backend/src/api/User.ts
@@ -1,70 +1,82 @@
-const express = require('express');
+import express from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User";
+import { Request, Response } from "express";
+import request from "superagent";
+
 const router = express.Router();
 
-// Mongoose user model
-const User = require('../models/User');
-
-// Password handler
-const bcrypt = require('bcrypt');
-
 // Signup
-router.post('/signup', (req: any, res: any) => {
-    let {email, password} = req.body;
+router.post("/signup", (req: Request, res: Response) => {
+  /*let {email, password} = req.body;
     email = email.trim();
-    password = password.trim();
+    password = password.trim();*/
 
-    if (!email || !password) {
-        return res.status(400).json({message: 'Invalid email or password!'});
-    } else if (password.length < 6) {
-        return res.status(400).json({message: 'Password must be at least 6 characters!'});
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        return res.status(400).json({message: 'Invalid email!'});
-    } else {
-        // Check if user already exists
-        User.find({ email }).then((result: any[]) => {
-            // Handle the result
-            if (result && result.length > 0) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Input email or password!" });
+  } else if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters!" });
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    return res.status(400).json({ message: "Invalid email!" });
+  } else {
+    // Check if user already exists
+    User.find({ email })
+      .then((result: any[]) => {
+        // Handle the result
+        if (result && result.length > 0) {
             // User already exists
-            return res.status(400).json({message: 'User already exists!'});
-            } else {
-            // Try to create new user
+            return res.status(400).json({ message: "User already exists!" });
+        } else {
+          // Try to create new user
 
-            // password handling
-            const saltRounds: number = 10;
-            bcrypt.hash(password, saltRounds)
-                .then((hashedPassword: string) => {
-                const newUser = new User({
-                    email,
-                    password: hashedPassword
-                });
+          // password handling
+          const saltRounds: number = 10;
+          bcrypt
+            .hash(password, saltRounds)
+            .then((hashedPassword: string) => {
+              const newUser = new User({
+                email,
+                password: hashedPassword,
+              });
 
-                newUser.save().then((result: any) => {
-                    return res.status(201).json({message: 'User created successfully!',
-                    data: result,
+              newUser
+                .save().then((result: any) => {
+                  return res
+                    .status(201)
+                    .json({
+                      message: "User created successfully!",
+                      data: result,
                     });
                 })
                 .catch((err: Error) => {
-                    console.log(err);
-                    return res.status(500).json({message: 'An error occurred while saving user!'});
+                  console.log(err);
+                  return res
+                    .status(500)
+                    .json({ message: "An error occurred while saving user!" });
                 });
-                })
-                .catch((err: Error) => {
-                console.log(err);
-                return res.status(500).json({ message: 'An error occurred while hashing password!' });
-                });
-            }
-        })
-        .catch((err: Error) => {
-            console.log(err);
-            return res.status(500).json({ message: 'Internal server error!' });
-        });
-    }
+            })
+            .catch((err: Error) => {
+              console.log(err);
+              return res
+                .status(500)
+                .json({ message: "An error occurred while hashing password!" });
+            });
+        }
+      })
+      .catch((err: Error) => {
+        console.log(err);
+        return res.status(500).json({ message: "Internal server error!" });
+      });
+  }
 });
 
-
 // Signin
-router.post('/signin', (req: any, res: any) => {
-    1
+router.post("/signin", (req: Request, res: Response) => {
+  1;
 });
 
 export default router;
