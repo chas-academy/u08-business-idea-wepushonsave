dbacf486c55b055bf10ec106ab837a401c59c153
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-05-20 13:09:31 +0100

    headlines/initial/commit

---
+++ frontend/src/components/navbar/game-docs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+<>
+
+<h2>“Overlook short form”</h2>
+<h2>PHILOSOPHY</h2>
+<h2>THE GOLDEN RULE</h2>
+<h2>HOUSE RULES</h2>
+<h2>RULES SOURCE</h2>
+<h2>DECK CONSTRUCTION</h2>
+<h2>STARTING GAME</h2>
+<h2>COMMANDER</h2>
+<h2>CARD LEGALITY</h2>
+<h2>BANNED LIST</h2>
+</>
