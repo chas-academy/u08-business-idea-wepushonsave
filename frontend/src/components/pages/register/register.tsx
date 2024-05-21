960de03f87530a671ed102773af2998b7ba08099
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-05-21 21:48:38 +0200

    Form created, no style

--- frontend/src/components/pages/register/register.tsx
+++ frontend/src/components/pages/register/register.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { Form } from 'react-router-dom';
+import { useState } from 'react';
 
-const register = () => {
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [register, setRegister] = useState(false);
+
+const registerUser = () => {
+
+    const handleSubmit = (e: any) => {
+        // prevent the form from refreshing the whole page
+        e.preventDefault();
+        // make a popup alert showing the "submitted" text
+        alert("Submited");
+      }
+
     return (
         <>
-        <h1>Register</h1>
+        
+           {/* email */}
+           <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e: any) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        {/* password */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e: any) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </Form.Group>
+
         </>
     )
 }
 
-export default register;
+export default registerUser;
