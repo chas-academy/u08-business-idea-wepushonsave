2609393f1d3c00727aeabbeaf34487518bb77b99
LloydElery <dennisgooglar@gmail.com>
2024-05-21 14:16:41 +0200

    Linting

--- frontend/src/components/card/CardInfo.tsx
+++ frontend/src/components/card/CardInfo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-//FIXME See if there is any other data-type we can use for an unknown value
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const CardInfo = ({card}: any) => {
+const CardInfo = () => {
+  //FIXME See if there is any other data-type we can use for an unknown value
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   /* eslint-enable @typescript-eslint/no-explicit-any */
   return (
     <>
--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {NavLink, Outlet} from 'react-router-dom';
 import App from '../App';
 
