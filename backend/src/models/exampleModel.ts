2512d449faaed63f467208cf7499bcb835b34ac8
Dennis Jensen <122142408+LloydElery@users.noreply.github.com>
2024-05-21 15:18:04 +0200

    Backend file structure and naming (#114)

---
+++ backend/src/models/exampleModel.ts
@@ -0,0 +1,8 @@
+/**
+ * This is just a placeholder and can be deleted whenever
+ *
+ * Use @camelCase to name files [camelCase](https://www.google.com/search?q=camelcase+vs+pascal+case&sca_esv=91bfb1353ebccd43&sca_upv=1&biw=1064&bih=1048&sxsrf=ADLYWIK_IVQEkF7MR7BWpAqDoJGYghXYWQ%3A1716297057791&ei=YZ1MZp_nL7DiwPAP-OueyA0&udm=&oq=camelcase+&gs_lp=Egxnd3Mtd2l6LXNlcnAiCmNhbWVsY2FzZSAqAggAMggQABiABBjLATIIEAAYgAQYywEyCBAAGIAEGMsBMggQABiABBjLATIIEAAYgAQYywEyBRAAGIAEMggQABiABBjLATIFEAAYgAQyBRAAGIAEMgUQABiABEjqGFAAWJwPcAB4AZABAJgBXaAByQWqAQIxMLgBA8gBAPgBAZgCCqAC8QXCAgoQIxiABBgnGIoFwgIMECMYgAQYExgnGIoFwgIKEAAYgAQYQxiKBcICDRAAGIAEGLEDGEMYigXCAhEQLhiABBixAxjRAxiDARjHAcICCxAAGIAEGLEDGIMBwgIEECMYJ8ICDhAuGIAEGLEDGNEDGMcBwgIFEC4YgATCAg4QLhiABBixAxiDARiKBcICCBAuGIAEGLEDwgIOEC4YgAQYxwEYjgUYrwHCAggQABiABBixA8ICCxAuGIAEGNQCGMsBwgIIEC4YgAQYywHCAgcQABiABBgKmAMAkgcCMTCgB6WUAQ&sclient=gws-wiz-serp#vhid=xdYzQC-9km5KRM&vssid=l)
+ * Add @Model at the end of your filename
+ * it is good practise to use a name similar to
+ * @models @controllers and @routes
+ */
