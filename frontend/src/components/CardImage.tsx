0c5a5190a568cd8bbfd4a8d6881c9951380188f8
LloydElery <dennisgooglar@gmail.com>
2024-05-21 14:16:41 +0200

    Linting

--- frontend/src/components/CardImage.tsx
+++ frontend/src/components/CardImage.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {useLoaderData} from 'react-router-dom';
 
 const CardImage = () => {
+  //FIXME See if there is any other data-type we can use for an unknown value
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   const cardData: any = useLoaderData();
+  /* eslint-enable @typescript-eslint/no-explicit-any */
   const cardImage = cardData.image_uris.border_crop;
 
   return (
--- frontend/src/components/card/CardMarket.tsx
+++ frontend/src/components/card/CardMarket.tsx
@@ -3,7 +3,10 @@ import {useLoaderData} from 'react-router-dom';
 import {convertObjectToArray} from '../../utils/convertObjectToArray';
 
 const CardMarket = () => {
+  //FIXME See if there is any other data-type we can use for an unknown value
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   const cardData: any = useLoaderData();
+  /* eslint-enable @typescript-eslint/no-explicit-any */
   const cardPrints = cardData.prints_search_uri;
   console.log(cardPrints);
   const cardPrices = convertObjectToArray(cardData.prices);
