adafd5493dbb36d68e000395e68256056a65fd11
LloydElery <dennisgooglar@gmail.com>
2024-05-21 14:16:41 +0200

    Linting

--- frontend/src/components/card/CardTestLegalities.tsx
+++ frontend/src/components/card/CardTestLegalities.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {useLoaderData} from 'react-router-dom';
 import {convertObjectToArray} from '../../utils/convertObjectToArray';
 
--- frontend/src/layouts/CardLayout.tsx
+++ frontend/src/layouts/CardLayout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {NavLink, Outlet} from 'react-router-dom';
 import CardImage from '../components/CardImage';
 
