fec182ba71e84b60d2e8ac97781ad457d119be05
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-05-22 11:39:17 +0100

    make it vissable in game rules

---
+++ frontend/src/pages/game-docs/Game_rules.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FAQ_Game_rules from '../../components/reusable_collapsable/FAQ_Game_rules';
+
+
+
+const GameRules = () => {
+  return (
+    <div className="bg-gray-600 p-4">
+      <div>
+        <FAQ_Game_rules />
+      </div>
+    </div>
+  );
+};
+export default GameRules;
