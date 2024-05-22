70518afdf6b97a46ea9230ff41817ae3a2e791e3
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-05-22 11:39:00 +0100

    too make it vissable in card rules

---
+++ frontend/src/pages/game-docs/Card_rules.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FAQ_Card_rules from '../../components/reusable_collapsable/FAQ_Card_rules';
+
+
+
+const CardRules = () => {
+  return (
+    <div className="bg-gray-600 p-4">
+      <div>
+        <FAQ_Card_rules />
+      </div>
+    </div>
+  );
+};
+export default CardRules;
