09ea7000bcd77f2630130c215c950724d59d4f30
Dennis Jensen <122142408+LloydElery@users.noreply.github.com>
2024-05-22 13:49:47 +0200

    117 single card (#125)
    * Removed 'Test' path and 'TestLegalities'
    
    Added background color to the temporary nav to display progress
    
    * Added a navlink to a page with multiple cards
    
    Created a loader function that fetches a set of cards
    
    Created a component that displays a card set

---
+++ frontend/src/components/card/CardsArray.tsx
@@ -0,0 +1,48 @@
+import {useLoaderData} from 'react-router-dom';
+
+/**
+ * @IAPIResponse
+ *  | @data : [ x { card-objects } ]
+ *    | @ICard : { key - value }
+
+ */
+
+// Defines the structure of image_uris
+interface IImageUris {
+  border_crop: string;
+}
+
+// Defines the structure of each card
+interface ICard {
+  name: string;
+  image_uris: IImageUris;
+}
+
+// Defines the structure for the API response
+interface IAPIResponse {
+  data: ICard[];
+}
+
+const CardsArray: React.FC = () => {
+  const apiResponse = useLoaderData() as IAPIResponse;
+
+  const cards = apiResponse.data;
+  console.log(cards);
+
+  if (cards !== null) {
+    return (
+      <>
+        <h1 className=" text-cyan-50">CardsArray.tsx</h1>
+        <div className="grid grid-cols-3">
+          {cards.map((value, index) => (
+            <div key={index}>
+              <img src={value.image_uris.border_crop} alt="" />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+};
+
+export default CardsArray;
