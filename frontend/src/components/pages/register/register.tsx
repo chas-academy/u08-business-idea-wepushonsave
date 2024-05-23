57332bf35c3c4ecba03fc6c2344b9fec75754848
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-05-23 11:21:20 +0200

    First commit

--- frontend/src/components/pages/register/register.tsx
+++ frontend/src/components/pages/register/register.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState } from "react";
+
+//import { useState } from "react";
 
 /*const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
