cae2ede2d1f5dda1b5058c71a953853ddf5e0f6a
LloydElery <dennisgooglar@gmail.com>
2024-05-23 10:33:41 +0200

    ESLint config, addad rule regardning 'react in jsx score'

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,17 +2,19 @@ module.exports = {
   root: true,
   env: {browser: true, es2020: true},
   extends: [
+    'plugin:react/jsx-runtime',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'plugin:jsx-ally/recommended',
     'airbnb-typescript-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'react', 'react-hooks', 'jsx-ally', 'prettier'],
   rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
