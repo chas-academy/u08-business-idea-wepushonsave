d48fc7870d18ebfb2438c05320033613410cbc69
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-05-23 13:32:16 +0200

    auth file created and test endpoints in app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,4 +14,14 @@ app.get("/", (req: Request, res: Response) => {
   res.send("MTG Vault Backend");
 });
 
+// free endpoint
+app.get("/free-endpoint", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
+
 export default app;
