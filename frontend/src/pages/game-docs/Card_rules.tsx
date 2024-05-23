7dfc5357e8bf7155e3df508750d5b0df13aac95f
Clara Prioux <prioux.clara@gmail.com>
2024-05-23 14:38:10 +0200

    react import issues

--- frontend/src/pages/game-docs/Card_rules.tsx
+++ frontend/src/pages/game-docs/Card_rules.tsx
@@ -1,7 +1,5 @@
 import FAQ_Card_rules from '../../components/reusable_collapsable/FAQ_Card_rules';
 
-
-
 const CardRules = () => {
   return (
     <div className="bg-gray-600 p-4">
--- frontend/src/pages/game-docs/Game_rules.tsx
+++ frontend/src/pages/game-docs/Game_rules.tsx
@@ -1,7 +1,5 @@
 import FAQ_Game_rules from '../../components/reusable_collapsable/FAQ_Game_rules';
 
-
-
 const GameRules = () => {
   return (
     <div className="bg-gray-600 p-4">
