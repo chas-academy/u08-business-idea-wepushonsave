9fb665b859f4d3b601dae359d8cc404931ca3a96
Clara Prioux <prioux.clara@gmail.com>
2024-05-23 14:38:10 +0200

    react import issues

--- frontend/src/components/reusable_collapsable/Collapsable.tsx
+++ frontend/src/components/reusable_collapsable/Collapsable.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 interface CollapsableProps {
   titles: string;
@@ -11,10 +11,11 @@ const Collapsable: React.FC<CollapsableProps> = ({titles, content}) => {
   return (
     <div className="py-2">
       <button
-        onClick={() => setCollapsableOpen(!collapsableOpen)} //onclick event som kommer att öppna eller stänga drop downen 
+        onClick={() => setCollapsableOpen(!collapsableOpen)} //onclick event som kommer att öppna eller stänga drop downen
         className="flex justify-between w-full">
         <span>{titles}</span>
-        {collapsableOpen ? <span>-</span> : <span>+</span>} {/*om öppen ska - skrivas ut om stängd + */}
+        {collapsableOpen ? <span>-</span> : <span>+</span>}{' '}
+        {/*om öppen ska - skrivas ut om stängd + */}
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
@@ -22,11 +23,11 @@ const Collapsable: React.FC<CollapsableProps> = ({titles, content}) => {
             ? 'grid-rows-[1fr] opacity-100' //vad som händer om den inte är öppen
             : 'grid-rows-[0fr] opacity-0' //vad som händer om den är öppen
         }`}>
-          {/* här under skapas layout men också ger en content till collapsable som aggerar pappa, inuti contentet lägger man sedan i items som då är dropdownsens information */}
+        {/* här under skapas layout men också ger en content till collapsable som aggerar pappa, inuti contentet lägger man sedan i items som då är dropdownsens information */}
         {collapsableOpen && (
-          <div className="mt-2 p-2 bg-gray-50 border border-gray-200 rounded"> 
-          {content} 
-        </div>
+          <div className="mt-2 p-2 bg-gray-50 border border-gray-200 rounded">
+            {content}
+          </div>
         )}
       </div>
     </div>
