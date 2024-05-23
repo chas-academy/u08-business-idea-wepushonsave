44d2d6ae34c433d8dcdefc9f6288c392aed65898
Alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-05-23 09:44:46 +0100

    108 game docs (#134)
    * headlines/initial/commit
    
    * switch place
    
    * made them vissable
    
    * collapsable layout
    
    * content for collapsable
    
    * card rules collapsable
    
    * game rules collapsable
    
    * too make it vissable in card rules
    
    * make it vissable in game rules
    
    * generating the collapse after the collaps layout
    
    * fungerande dropdown, med content för game rules
    
    * headlines/initial/commit
    
    * switch place
    
    * collapsable layout
    
    * content for collapsable
    
    * card rules collapsable
    
    * game rules collapsable
    
    * too make it vissable in card rules
    
    * make it vissable in game rules
    
    * generating the collapse after the collaps layout
    
    * fungerande dropdown, med content för game rules

---
+++ frontend/src/components/reusable_collapsable/generateCollapsableContent.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import CollapsableContent from './CollapsableContent';
+
+interface CollapsibleSection {
+  title: string;
+  items: string[];
+}
+
+const generateCollapsableContent = (sections: CollapsibleSection[]) => {
+  return (
+    <>
+      {sections.map((section, index) => ( //uprepar varje sections array med map function, key hjälper react att updatera dom. inuti dem skapade divarna skapas collapsablesection och content med title och items
+        <div key={index} id={`collapsible-section-${index}`}> 
+          <CollapsableContent 
+            title={section.title}
+            items={section.items}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default generateCollapsableContent;
