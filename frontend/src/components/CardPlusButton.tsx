2097a644c8a1076b4ea53a3b638c9a6cc0751db7
LloydElery <dennisgooglar@gmail.com>
2024-05-24 13:59:38 +0200

    CardFooter

--- frontend/src/components/CardPlusButton.tsx
+++ frontend/src/components/CardPlusButton.tsx
@@ -5,8 +5,8 @@ const CardPlusButton = () => {
     <>
       {/* hover:shadow-plum hover:bg-mint/60 */}
       {/* hover:border-white */}
-      <div className="card-plus-button-container w-full min-h-12 grid grid-cols-1 justify-items-end content-center">
-        <button className="plus-button-circle rounded-full size-10 bg-btn-gradient shadow-md hover:shadow-lg hover:shadow-plum hover:bg-mint/60">
+      <div className="card-plus-button-container w-full min-h-12 grid grid-cols-1 justify-items-center ">
+        <button className="plus-button-circle rounded-full size-8 bg-btn-gradient shadow-md hover:shadow-lg hover:shadow-plum hover:bg-mint/60 ml-16 mt-1">
           <img src={plusButton} alt="add-button" />
         </button>
       </div>
--- frontend/src/components/card/CardValue.tsx
+++ frontend/src/components/card/CardValue.tsx
@@ -1,7 +1,7 @@
 const CardValue = () => {
   return (
     <>
-      <div className="card-value-container  w-full min-h-12 grid grid-cols-1 justify-items-start content-center">
+      <div className="card-value-container  w-full min-h-12 grid grid-cols-1 justify-items-center ">
         <h1 className="card-calue-text text-xl">Card value: $</h1>
       </div>
     </>
