712b11ed28535343c406db87d6b2a8e3d54c3b80
LloydElery <dennisgooglar@gmail.com>
2024-05-25 20:56:46 +0200

    - Display 'Set_name' - Display price and € - Display illustration in a card container

--- frontend/src/components/card/CardMarket.tsx
+++ frontend/src/components/card/CardMarket.tsx
@@ -1,7 +1,7 @@
 import {useLoaderData} from 'react-router-dom';
 import {convertObjectToArray} from '../../utils/convertObjectToArray';
 import {useEffect, useState} from 'react';
-import {IPrintsAPIResponse, IPrintsData} from './IPrints';
+import {IPrices, IPrintsAPIResponse, IPrintsData} from './IPrints';
 
 const delay = async (ms: number) => {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -18,6 +18,7 @@ const CardMarket = () => {
   const [prints, setPrints] = useState<IPrintsData[]>([]);
 
   const cardPrices = convertObjectToArray(cardData.prices);
+  console.log(cardPrices);
   const cardMarkets = convertObjectToArray(cardData.purchase_uris);
   const cardPrints = cardData.prints_search_uri.toString();
 
@@ -35,10 +36,31 @@ const CardMarket = () => {
     };
     getPrints();
   }, []);
+
+  const getEURPrice = (prices: IPrices): string | null => {
+    const price = prices.eur || prices.eur_foil || null;
+    return price ? `${price} €` : 'No price available';
+  };
+
   return (
     <>
+      {/* .map(print) = The amount of diffrent card illustarations  */}
       {prints.map(print => (
-        <img src={print.image_uris.border_crop} alt="" />
+        <div className="card-market-prints-container border border-red-600 grid grid-cols-3 gap-1">
+          <div className="print-container m-2">
+            <img src={print.image_uris.border_crop} alt="" />
+          </div>
+          <div className="print-details-container col-span-2 justify-items-center content-center ">
+            <p>
+              <strong className="m-2">SET:</strong>
+              {print.set_name}
+            </p>
+            <p>
+              <strong className="m-2">PRICE:</strong>
+              {getEURPrice(print.prices)}
+            </p>
+          </div>
+        </div>
       ))}
       <div className="grid grid-cols-2 bg-[#322929] gap-1">
         {cardPrices.map(({key, value}, index) => (
