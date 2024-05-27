e799342e880bcbec8763540323255b6766c46ea5
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-05-27 12:10:27 +0100

    newer standard

--- frontend/src/components/reusable_collapsable/Collapsable.tsx
+++ frontend/src/components/reusable_collapsable/Collapsable.tsx
@@ -1,20 +1,22 @@
-import React, {useState} from 'react';
+/* eslint-disable react/react-in-jsx-scope */
+import {useState} from 'react';
 
 interface CollapsableProps {
   titles: string;
-  content: React.ReactNode;
+  content: string;
 }
 
-const Collapsable: React.FC<CollapsableProps> = ({titles, content}) => {
+const Collapsable = ({titles, content}: CollapsableProps) => {
   const [collapsableOpen, setCollapsableOpen] = useState(false);
 
   return (
     <div className="py-2">
       <button
-        onClick={() => setCollapsableOpen(!collapsableOpen)} //onclick event som kommer att öppna eller stänga drop downen 
+        onClick={() => setCollapsableOpen(!collapsableOpen)} //onclick event som kommer att öppna eller stänga drop downen
         className="flex justify-between w-full">
-        <span className='font-semibold'>{titles}</span>
-        {collapsableOpen ? <span>-</span> : <span>+</span>} {/*om öppen ska - skrivas ut om stängd + */}
+        <span className="font-semibold">{titles}</span>
+        {collapsableOpen ? <span>-</span> : <span>+</span>}{' '}
+        {/*om öppen ska - skrivas ut om stängd + */}
       </button>
       <div
         className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
@@ -22,11 +24,9 @@ const Collapsable: React.FC<CollapsableProps> = ({titles, content}) => {
             ? 'grid-rows-[1fr] opacity-100' //vad som händer om den inte är öppen
             : 'grid-rows-[0fr] opacity-0' //vad som händer om den är öppen
         }`}>
-          {/* här under skapas layout men också ger en content till collapsable som aggerar pappa, inuti contentet lägger man sedan i items som då är dropdownsens information */}
+        {/* här under skapas layout men också ger en content till collapsable som aggerar pappa, inuti contentet lägger man sedan i items som då är dropdownsens information */}
         {collapsableOpen && (
-          <div className="mt-2 p-2 bg-zinc-700 rounded"> 
-          {content} 
-        </div>
+          <div className="mt-2 p-2 bg-zinc-700 rounded">{content}</div>
         )}
       </div>
     </div>
