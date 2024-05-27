1a74d52279e4690ecfacf6818d90b4a056708cec
Emma <133673994+lemonyblossom@users.noreply.github.com>
2024-05-27 20:46:24 +0200

    181 home page imgstyling (#194)
    * imported bg for mobile and desktop search component on homepage
    
    * mobile and desktop hero as background image for search-form, only displays if url isHome

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import GameRules from './pages/game-docs/Game_rules.tsx';
 import CardRules from './pages/game-docs/Card_rules.tsx';
 import Threads from './pages/community/Threads.tsx';
 
+
 // components
 import CardInfo from './components/card/CardInfo.tsx';
 import CardLegalities from './components/card/CardLegalities.tsx';
@@ -25,14 +26,14 @@ import RootLayout from './layouts/RootLayout.tsx';
 import CardLayout from './layouts/CardLayout.tsx';
 
 // utils
-import {singleCardLoader} from './utils/singleCardLoader.tsx';
-import {cardsArrayLoader} from './utils/cardsArrayLoader.tsx';
-import {cardSetLoader} from './utils/cardSetLoader.tsx';
+import { singleCardLoader } from './utils/singleCardLoader.tsx';
+import { cardsArrayLoader } from './utils/cardsArrayLoader.tsx';
+import { cardSetLoader } from './utils/cardSetLoader.tsx';
 import SearchResults from './components/search/SearchResults.tsx';
-import {cardSearchLoader} from './utils/cardSearchLoader.tsx';
+import { cardSearchLoader } from './utils/cardSearchLoader.tsx';
 
 // context
-import {SearchProvider} from './components/search/SearchContext.tsx';
+import { SearchProvider } from './components/search/SearchContext.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -47,8 +48,8 @@ const router = createBrowserRouter(
       <Route
         path="cards/:set"
         element={<CardsArray />}
-        loader={({params}) => {
-          return cardSetLoader({params});
+        loader={({ params }) => {
+          return cardSetLoader({ params });
         }}
       />
       <Route path="gamerules" element={<GameRules />} />
@@ -58,28 +59,28 @@ const router = createBrowserRouter(
       <Route
         path="card/:id"
         element={<CardLayout />}
-        loader={({params}) => {
-          return singleCardLoader({params});
+        loader={({ params }) => {
+          return singleCardLoader({ params });
         }}>
         <Route
           path="info"
           element={<CardInfo />}
-          loader={({params}) => {
-            return singleCardLoader({params});
+          loader={({ params }) => {
+            return singleCardLoader({ params });
           }}
         />
         <Route
           path="market"
           element={<CardMarket />}
-          loader={({params}) => {
-            return singleCardLoader({params});
+          loader={({ params }) => {
+            return singleCardLoader({ params });
           }}
         />
         <Route
           path="legalities"
           element={<CardLegalities />}
-          loader={({params}) => {
-            return singleCardLoader({params});
+          loader={({ params }) => {
+            return singleCardLoader({ params });
           }}
         />
       </Route>
---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+   return (
+      <h2>THIS IS THE HOMEPAGE</h2>
+   );
+};
+
+export default Home;
