2fae9bf8bdef2ee1ead8164303c7f38edc5e5336
Clara Prioux <prioux.clara@gmail.com>
2024-05-28 21:44:49 +0200

    just commented out the variables, lolo is fixing it now, removed navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
-import Navbar from './components/navbar/Navbar';
 import Login from './pages/login/Login';
 //import Register from './pages/register/Register';
-
-
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-     
       <Login></Login>
       {/*<Register></Register>*/}
     </>
