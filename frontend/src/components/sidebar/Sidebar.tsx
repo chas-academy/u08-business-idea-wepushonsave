816ad557a3739c6f133d6c59bc9fd4f3a50a25cd
Emma Jensen <emmasarahlinnea@gmail.com>
2024-05-28 13:25:43 +0200

    adjusted sidebar to have margin in relation to the nav depending on mobile or desktop

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -13,8 +13,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
    };
 
    const iconStyle = {
-      transitionDuration: isOpen ? '1000ms' : '200ms',
-      transform: isOpen ? 'rotate(90deg)' : 'rotate(0)',
+      transitionDuration: isOpen ? '1500ms' : '200ms',
+      transform: isOpen ? 'rotate(360deg)' : 'rotate(90deg)',
       borderRadius: '30%',
       padding: isOpen ? '3px' : '10px',
       transitionDelay: isOpen ? '200ms' : '0',
@@ -28,7 +28,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
    };
 
    return (
-      <div className="fixed inset-0 z-50 flex" style={sidebarStyle}>
+      <div className="fixed inset-0 z-50 flex mb-[68px] md:mb-0 md:mt-[80px]" style={sidebarStyle}>
          <div className="bg-gray-700 text-white w-64 p-4 relative">
             <div className="flex justify-end">
                <button onClick={toggleSidebar} className={`absolute top-2 right-2 p-2 rounded-full ${isOpen ? 'border-2 border-orange-200  bg-white/30' : ''}`} style={buttonStyle}>
@@ -38,10 +38,9 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
             </div>
             <nav className="mt-12">
                <ul>
-                  <li className="py-2"><a href="#">Home</a></li>
                   <li className="py-2"><a href="#">About</a></li>
-                  <li className="py-2"><a href="#">Services</a></li>
-                  <li className="py-2"><a href="#">Contact</a></li>
+                  <li className="py-2"><a href="#">Game Rules</a></li>
+                  <li className="py-2"><a href="#">Card Rules</a></li>
                </ul>
             </nav>
          </div>
