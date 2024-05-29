42f944e6fdf5741248ab8ebbc69473d7136d0502
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-05-29 22:19:07 +0200

    Login and reg styled like profile dashboard

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -2,10 +2,15 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */ // Line 42
 
 import {useState} from 'react';
-import Validation from './loginValidation';
+import Validation from './loginValidation'
 
+import { useNavigate } from 'react-router-dom';
 
 const LoginUser = () => {
+  const navigate = useNavigate() 
+  const navigateHome = async() => {
+    navigate("/Register")
+   }
   const [values, setValues] = useState({
     email: '',
     password: '',
@@ -26,15 +31,13 @@ const LoginUser = () => {
         method: 'POST',
         mode: 'cors',
         headers: {
-          'Content-type': 'application/json'
+          'Content-type': 'application/json',
         },
         credentials: 'include',
-        body: JSON.stringify(values)
-      }).then(
-        (response) => {
-          console.log(response.headers)
-        }
-      );
+        body: JSON.stringify(values),
+      }).then(response => {
+        console.log(response.headers);
+      });
     } else {
       console.log('Errors');
     }
@@ -48,75 +51,57 @@ const LoginUser = () => {
 
   return (
     <>
-      <div className="">
-        <h1 className="">Login</h1>
-        <div className="">
-          <form action="" onSubmit={handleSubmit}>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className=""
-              placeholder="Email"
-              onChange={handleInput}
-            />
-            {errors.email && (
-              <span className="text-danger"> {errors.email} </span>
-            )}
-
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className=""
-              placeholder="Password"
-              onChange={handleInput}
-            />
-            {errors.password && (
-              <span className="text-danger"> {errors.password} </span>
-            )}
-
-            <button
-              type="submit"
-              className="mt-4 px-4 py-2 bg-purple-200 text-white rounded-md">
-              Login
-            </button>
+<div className="flex flex-col items-center justify-center min-h-screen text-white">
+      <h1 className="text-3xl md:text-5xl md:mb-20 font-bold mb-4">
+        Login
+      </h1>
+      <div className="flex flex-col md:w-1/3 w-5/6 p-5 rounded-xl shadow-lg bg-profile-content">
+        
+        <form onSubmit={handleSubmit}>
+          <ul className="mb-6 space-y-4">
+            <li className=" font-bold">
+              Email adress:
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full p-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
+                  placeholder="Email"
+                  onChange={handleInput}
+                /> 
+                {errors.email && (
+                  <span className="text-danger"> {errors.email} </span>
+                )}
+                </li>
+                <li className=" font-bold">
+                  Password:
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full p-2 mt-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
+                  placeholder="Password"
+                  onChange={handleInput}
+                />
+                {errors.password && (
+                  <span className="text-danger"> {errors.password} </span>
+                )}
+              </li>
+              </ul>
+              <div className="flex justify-end">
+              <button
+                type="submit"
+                className="px-4 py-1 text-white rounded-md hover:text-aubergine bg-btn-gradient">
+                Login
+              </button>
+            </div>
           </form>
+          <h2
+          onClick={() => navigateHome()}
+          >Not registred yet? Click here!</h2>
         </div>
       </div>
     </>
   );
 };
 export default LoginUser;
-
-/*
-<div className="relative w-64 h-64 mx-auto">
-  <div className="w-full h-full bg-purple-600 rounded-md"></div> <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-    <h1 className="text-3xl font-bold text-gray-800">Login</h1> </div>
-
-  <div className="absolute bottom-0 left-0 w-full h-48 flex flex-col items-center">
-    <div className="w-full p-4 bg-purple-600 rounded-md">
-    <form action="" onSubmit={handleSubmit}>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        className="w-full p-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
-        placeholder="Email"
-        onChange={handleInput} />
-      <input
-        type="password"
-        id="password"
-        name="password"
-        className="w-full p-2 mt-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
-        placeholder="Password"
-        onChange={handleInput} />
-    
-
-    <button type="submit" className="mt-4 px-4 py-2 bg-purple-200 text-white rounded-md">Login</button>
-    </form>
-    </div>
-  </div>
-</div>
-
-*/
--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 const RegisterUser = () => {
+  /*const navigate = useNavigate() 
+  const navigateHome = async() => {
+    navigate("/Register")
+   }*/
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget;
@@ -28,85 +32,63 @@ const RegisterUser = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Register</h1>
-      <form
-        action=""
-        onSubmit={handleSubmit}
-        className="flex justify-center items-center h-screen">
-        <div className="bg-white rounded-lg shadow-md px-8 py-6 flex flex-col space-y-4">
-          <div className="flex flex-col">
-            <input
-              type="text"
-              name="username"
-              className="appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              placeholder="Username"
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <input
-              type="email"
-              name="email"
-              className="appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              placeholder="Email"
-            />
-          </div>
-
-          <div className="flex flex-col">
-            <input
-              type="password"
-              name="password"
-              className="appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              placeholder="Password"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:offset-2 focus:ring-indigo-500">
-            Submit
-          </button>
+  <div className="flex flex-col items-center justify-center min-h-screen text-white">
+      <h1 className="text-3xl md:text-5xl md:mb-20 font-bold mb-4">
+        Login
+      </h1>
+      <div className="flex flex-col md:w-1/3 w-5/6 p-5 rounded-xl shadow-lg bg-profile-content">
+        
+        <form onSubmit={handleSubmit}>
+          <ul className="mb-6 space-y-4">
+          <li className=" font-bold">
+              Username:
+                <input
+                  type="text"
+                  name="username"
+                  className="appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  placeholder="Username"
+                />
+              </li>
+            <li className=" font-bold">
+              Email adress:
+                <input
+                  type="email"
+                  
+                  name="email"
+                  className="w-full p-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
+                  placeholder="Email"
+                  
+                /> 
+               
+                </li>
+                <li className=" font-bold">
+                  Password:
+                <input
+                  type="password"
+                  
+                  name="password"
+                  className="w-full p-2 mt-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
+                  placeholder="Password"
+                  
+                />
+            
+              </li>
+              </ul>
+              <div className="flex justify-end">
+              <button
+                type="submit"
+                className="px-4 py-1 text-white rounded-md hover:text-aubergine bg-btn-gradient">
+                Login
+              </button>
+            </div>
+          </form>
+          <h2
+          //onClick={() => navigateHome()}
+          >Not registred yet? Click here!</h2>
         </div>
-      </form>
+      </div>
     </>
   );
 };
 
 export default RegisterUser;
-
-/*
-<h1 className="text-3xl font-bold text-center">Register</h1>
-      <form action="" onSubmit={handleSubmit}
-      className="flex flex-col space-y-4">
-        <div className="flex flex-col">
-          <label htmlFor="email" className="text-sm font-medium mb-2">
-            Email address
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Email"            
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label htmlFor="password" className="text-sm font-medium mb-2">
-            Choose a password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Password"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Submit
-        </button>
-      </form>
-*/
