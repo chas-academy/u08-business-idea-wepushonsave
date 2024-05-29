73e2c317e899b3527e4a00dfaa165daf7d6af510
Emma Jensen <emmasarahlinnea@gmail.com>
2024-05-29 05:47:04 +0200

    hover, tansform, list style

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -45,7 +45,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
 
    return (
       <div className="fixed inset-0 z-20 flex mb-[68px] md:mb-0 md:mt-[80px]" style={sidebarStyle}>
-         <div className="bg-plum/90 text-white/80 w-64 p-4 relative">
+         <div className="bg-zinc-900/95 text-white/70 w-64 p-4 relative">
             <div className="flex justify-end">
                <button onClick={toggleSidebar} className={`absolute top-2 right-2 p-1 rounded-full ${isOpen ? 'border border-purple-100/20 bg-periwinkle/70' : ''}`} style={buttonStyle}>
                   <img src={docIcon} alt="Doc Icaon" className="w-10 h-10" style={iconStyle} />
@@ -55,77 +55,88 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
                <div className="mt-4">
                   <ul>
                      <li className="ABOUT py-2">
-                        <button type="button" onClick={() => handleExpand('about')}>
-                           About
+                        <button
+                           type="button"
+                           onClick={() => handleExpand('about')}
+                           className={`hover:text-white ${expandedItem === 'about' ? 'text-white' : ''}`}
+                        >                           About
                         </button>
 
-                        <div className={`smooth ${expandedItem === 'about' ? 'max-h-[1000px]' : 'max-h-0'}`} style={{ transition: `max-height ${expandedItem === 'about' ? '800ms' : '0ms'} ease-in-out`, overflow: 'hidden' }}>
-                           <ul className="About bg-white/10 px-2 max-h-[calc(100vh-300px)] overflow-y-auto" style={scrollbarStyle}>
-                              <li className="py-1 text-sm"><a href="#A1">About 1</a></li>
-                              <li className="py-1 text-sm"><a href="#A2">About 2</a></li>
-                              <li className="py-1 text-sm"><a href="#A3">About 3</a></li>
+                        <div className={`smooth ${expandedItem === 'about' ? 'max-h-[1000px]' : 'max-h-0'}`} style={{ transition: `max-height ${expandedItem === 'about' ? '800ms' : '300ms'} ease-in-out`, overflow: 'hidden' }}>
+                           <ul className="About bg-white/10 rounded-md p-2 max-h-[calc(100vh-300px)] overflow-y-auto" style={scrollbarStyle}>
+                              <li className="py-1 text-sm"><a href="#A1" className="hover:text-white">About 1</a></li>
+                              <li className="py-1 text-sm"><a href="#A2" className="hover:text-white">About 2</a></li>
+                              <li className="py-1 text-sm"><a href="#A3" className="hover:text-white">About 3</a></li>
+
                            </ul>
                         </div>
                      </li>
 
                      <li className="GAME RULES py-2">
-                        <button type="button" onClick={() => handleExpand('gameRules')}>
-                           Game Rules
+                        <button
+                           type="button"
+                           onClick={() => handleExpand('gameRules')}
+                           className={`hover:text-white ${expandedItem === 'gameRules' ? 'text-white' : ''}`}
+                        >                           Game Rules
                         </button>
 
-                        <div className={`smooth ${expandedItem === 'gameRules' ? 'max-h-[1000px]' : 'max-h-0'}`} style={{ transition: `max-height ${expandedItem === 'gameRules' ? '800ms' : '0ms'} ease-in-out`, overflow: 'hidden' }}>
-                           <ul className="GameRules bg-white/10 px-2 max-h-[calc(100vh-300px)] overflow-y-auto" style={scrollbarStyle}>
-                              <li className="py-1 text-sm"><a href="#commander-rules">Commander Rules and Gameplay Guide</a></li>
-                              <li className="py-1 text-sm"><a href="#card-types">Card Types</a></li>
-                              <li className="py-1 text-sm"><a href="#turn-structure">Turn Structure</a></li>
-                              <li className="py-1 text-sm"><a href="#casting-spells">Casting Spells</a></li>
-                              <li className="py-1 text-sm"><a href="#combat">Combat</a></li>
-                              <li className="py-1 text-sm"><a href="#damage-and-life">Damage and Life</a></li>
-                              <li className="py-1 text-sm"><a href="#keywords-and-abilities">Keywords and Abilities</a></li>
-                              <li className="py-1 text-sm"><a href="#win-conditions">Win Conditions</a></li>
-                              <li className="py-1 text-sm"><a href="#interaction-and-stack">Interaction and Stack</a></li>
-                              <li className="py-1 text-sm"><a href="#mulligans-and-sideboarding">Mulligans and Sideboarding</a></li>
-                              <li className="py-1 text-sm"><a href="#philosophy">Philosophy</a></li>
-                              <li className="py-1 text-sm"><a href="#the-golden-rule">The Golden Rule</a></li>
-                              <li className="py-1 text-sm"><a href="#house-rules">House Rules</a></li>
-                              <li className="py-1 text-sm"><a href="#rules-sources">Rules Sources</a></li>
-                              <li className="py-1 text-sm"><a href="#deck-construction">Deck Construction</a></li>
-                              <li className="py-1 text-sm"><a href="#starting-the-game">Starting the Game</a></li>
-                              <li className="py-1 text-sm"><a href="#commander">Commander</a></li>
-                              <li className="py-1 text-sm"><a href="#card-legality">Card Legality</a></li>
-                              <li className="py-1 text-sm"><a href="#banned-list">Banned List</a></li>
-                              <li className="py-1 text-sm"><a href="#color-identity-and-color-indicator">Color Identity and Color Indicator</a></li>
-                              <li className="py-1 text-sm"><a href="#limited-range-of-influence">Limited Range of Influence</a></li>
-                              <li className="py-1 text-sm"><a href="#official-announcements-and-rulings">Official Announcements and Rulings</a></li>
-                              <li className="py-1 text-sm"><a href="#object-of-commander">Object of Commander</a></li>
-                              <li className="py-1 text-sm"><a href="#rule-violations">Rule Violations</a></li>
-                              <li className="py-1 text-sm"><a href="#additional-rules-for-commander-games">Additional Rules for Commander Games</a></li>
-                              <li className="py-1 text-sm"><a href="#faq-frequently-asked-questions">FAQ (Frequently Asked Questions)</a></li>
+                        <div className={`smooth ${expandedItem === 'gameRules' ? 'max-h-[1000px]' : 'max-h-0'}`} style={{ transition: `max-height ${expandedItem === 'gameRules' ? '800ms' : '300ms'} ease-in-out`, overflow: 'hidden' }}>
+                           <ul className="GameRules bg-white/10 rounded-md p-2 max-h-[calc(100vh-300px)] overflow-y-auto" style={scrollbarStyle}>
+                              <li className="py-1 text-sm"><a href="#commander-rules" className="hover:text-white">Commander Rules and Gameplay Guide</a></li>
+                              <li className="py-1 text-sm"><a href="#card-types" className="hover:text-white">Card Types</a></li>
+                              <li className="py-1 text-sm"><a href="#turn-structure" className="hover:text-white">Turn Structure</a></li>
+                              <li className="py-1 text-sm"><a href="#casting-spells" className="hover:text-white">Casting Spells</a></li>
+                              <li className="py-1 text-sm"><a href="#combat" className="hover:text-white">Combat</a></li>
+                              <li className="py-1 text-sm"><a href="#damage-and-life" className="hover:text-white">Damage and Life</a></li>
+                              <li className="py-1 text-sm"><a href="#keywords-and-abilities" className="hover:text-white">Keywords and Abilities</a></li>
+                              <li className="py-1 text-sm"><a href="#win-conditions" className="hover:text-white">Win Conditions</a></li>
+                              <li className="py-1 text-sm"><a href="#interaction-and-stack" className="hover:text-white">Interaction and Stack</a></li>
+                              <li className="py-1 text-sm"><a href="#mulligans-and-sideboarding" className="hover:text-white">Mulligans and Sideboarding</a></li>
+                              <li className="py-1 text-sm"><a href="#philosophy" className="hover:text-white">Philosophy</a></li>
+                              <li className="py-1 text-sm"><a href="#the-golden-rule" className="hover:text-white">The Golden Rule</a></li>
+                              <li className="py-1 text-sm"><a href="#house-rules" className="hover:text-white">House Rules</a></li>
+                              <li className="py-1 text-sm"><a href="#rules-sources" className="hover:text-white">Rules Sources</a></li>
+                              <li className="py-1 text-sm"><a href="#deck-construction" className="hover:text-white">Deck Construction</a></li>
+                              <li className="py-1 text-sm"><a href="#starting-the-game" className="hover:text-white">Starting the Game</a></li>
+                              <li className="py-1 text-sm"><a href="#commander" className="hover:text-white">Commander</a></li>
+                              <li className="py-1 text-sm"><a href="#card-legality" className="hover:text-white">Card Legality</a></li>
+                              <li className="py-1 text-sm"><a href="#banned-list" className="hover:text-white">Banned List</a></li>
+                              <li className="py-1 text-sm"><a href="#color-identity-and-color-indicator" className="hover:text-white">Color Identity and Color Indicator</a></li>
+                              <li className="py-1 text-sm"><a href="#limited-range-of-influence" className="hover:text-white">Limited Range of Influence</a></li>
+                              <li className="py-1 text-sm"><a href="#official-announcements-and-rulings" className="hover:text-white">Official Announcements and Rulings</a></li>
+                              <li className="py-1 text-sm"><a href="#object-of-commander" className="hover:text-white">Object of Commander</a></li>
+                              <li className="py-1 text-sm"><a href="#rule-violations" className="hover:text-white">Rule Violations</a></li>
+                              <li className="py-1 text-sm"><a href="#additional-rules-for-commander-games" className="hover:text-white">Additional Rules for Commander Games</a></li>
+                              <li className="py-1 text-sm"><a href="#faq-frequently-asked-questions" className="hover:text-white">FAQ (Frequently Asked Questions)</a></li>
                            </ul>
                         </div>
                      </li>
                      <li className="CARD RULES py-2">
-                        <button type="button" onClick={() => handleExpand('cardRules')}>
-                           Card Rules
+                        <button
+                           type="button"
+                           onClick={() => handleExpand('cardRules')}
+                           className={`hover:text-white ${expandedItem === 'cardRules' ? 'text-white' : ''}`}
+                        >                           Card Rules
                         </button>
 
-                        <div className={`smooth ${expandedItem === 'cardRules' ? 'max-h-[1000px]' : 'max-h-0'}`} style={{ transition: `max-height ${expandedItem === 'cardRules' ? '800ms' : '0ms'} ease-in-out`, overflow: 'hidden' }}>
-                           <ul className="CardRules bg-white/10 px-2 max-h-[calc(100vh-300px)] overflow-y-auto" style={scrollbarStyle}>
-                              <li className="py-1 text-sm"><a href="#land-types">Land Types</a></li>
-                              <li className="py-1 text-sm"><a href="#swamp">Swamp</a></li>
-                              <li className="py-1 text-sm"><a href="#mountain">Mountain</a></li>
-                              <li className="py-1 text-sm"><a href="#plains">Plains</a></li>
-                              <li className="py-1 text-sm"><a href="#forest">Forest</a></li>
-                              <li className="py-1 text-sm"><a href="#island">Island</a></li>
-                              <li className="py-1 text-sm"><a href="#colorless">Colorless</a></li>
-                              <li className="py-1 text-sm"><a href="#cave">Cave</a></li>
-                              <li className="py-1 text-sm"><a href="#desert">Desert</a></li>
-                              <li className="py-1 text-sm"><a href="#gate">Gate</a></li>
-                              <li className="py-1 text-sm"><a href="#lair">Lair</a></li>
-                              <li className="py-1 text-sm"><a href="#locus">Locus</a></li>
-                              <li className="py-1 text-sm"><a href="#sphere">Sphere</a></li>
-                              <li className="py-1 text-sm"><a href="#urzas">Urza's</a></li>
-                              <li className="py-1 text-sm"><a href="#cloud">Cloud</a></li>
+                        <div className={`smooth ${expandedItem === 'cardRules' ? 'max-h-[1000px]' : 'max-h-0'}`} style={{ transition: `max-height ${expandedItem === 'cardRules' ? '800ms' : '300ms'} ease-in-out`, overflow: 'hidden' }}>
+                           <ul className="CardRules bg-white/10 rounded-md p-2 max-h-[calc(100vh-300px)] overflow-y-auto" style={scrollbarStyle}>
+                              <li className="py-1 text-sm"><a href="#land-types" className="hover:text-white">Land Types</a></li>
+                              <li className="py-1 text-sm"><a href="#swamp" className="hover:text-white">Swamp</a></li>
+                              <li className="py-1 text-sm"><a href="#mountain" className="hover:text-white">Mountain</a></li>
+                              <li className="py-1 text-sm"><a href="#plains" className="hover:text-white">Plains</a></li>
+                              <li className="py-1 text-sm"><a href="#forest" className="hover:text-white">Forest</a></li>
+                              <li className="py-1 text-sm"><a href="#island" className="hover:text-white">Island</a></li>
+                              <li className="py-1 text-sm"><a href="#colorless" className="hover:text-white">Colorless</a></li>
+                              <li className="py-1 text-sm"><a href="#cave" className="hover:text-white">Cave</a></li>
+                              <li className="py-1 text-sm"><a href="#desert" className="hover:text-white">Desert</a></li>
+                              <li className="py-1 text-sm"><a href="#gate" className="hover:text-white">Gate</a></li>
+                              <li className="py-1 text-sm"><a href="#lair" className="hover:text-white">Lair</a></li>
+                              <li className="py-1 text-sm"><a href="#locus" className="hover:text-white">Locus</a></li>
+                              <li className="py-1 text-sm"><a href="#sphere" className="hover:text-white">Sphere</a></li>
+                              <li className="py-1 text-sm"><a href="#urzas" className="hover:text-white">Urza's</a></li>
+                              <li className="py-1 text-sm"><a href="#cloud" className="hover:text-white">Cloud</a></li>
+
                            </ul>
                         </div>
                      </li>
