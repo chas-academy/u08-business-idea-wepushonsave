3b4f700618676b441fe405604ecbae331805d4f9
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-05-30 11:34:37 +0200

    Login and register, redirect works, error message back and front works correct

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -2,15 +2,14 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */ // Line 42
 
 import {useState} from 'react';
-import Validation from './loginValidation'
+import Validation from './loginValidation';
 
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 const LoginUser = () => {
-  const navigate = useNavigate() 
-  const navigateHome = async() => {
-    navigate("/Register")
-   }
+  
+  const navigate = useNavigate();
+
   const [values, setValues] = useState({
     email: '',
     password: '',
@@ -19,28 +18,37 @@ const LoginUser = () => {
     email: '',
     password: '',
   });
+  const [loginError, setLoginError] = useState("test")
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
 
     const errorCheck = Validation(values);
     setErrors(errorCheck);
 
     if (!errorCheck.email && !errorCheck.password) {
-      fetch('http://localhost:3000/api/user/login', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        credentials: 'include',
-        body: JSON.stringify(values),
-      }).then(response => {
-        console.log(response.headers);
-      });
+      try {
+        const response = await fetch('http://localhost:3000/api/user/login', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify(values),
+        });
+        if (!response.ok) {
+          throw new Error('Login not successful, please try again');
+        }
+        navigate('/');
+      } catch (error: any) {
+        console.log('Login not successful', error);
+        setLoginError(error.message)
+      }
     } else {
       console.log('Errors');
     }
+    
   };
   const handleInput = (event: any) => {
     setValues(prev => ({
@@ -51,16 +59,13 @@ const LoginUser = () => {
 
   return (
     <>
-<div className="flex flex-col items-center justify-center min-h-screen text-white">
-      <h1 className="text-3xl md:text-5xl md:mb-20 font-bold mb-4">
-        Login
-      </h1>
-      <div className="flex flex-col md:w-1/3 w-5/6 p-5 rounded-xl shadow-lg bg-profile-content">
-        
-        <form onSubmit={handleSubmit}>
-          <ul className="mb-6 space-y-4">
-            <li className=" font-bold">
-              Email adress:
+      <div className="flex flex-col items-center justify-center min-h-screen text-white">
+        <h1 className="text-3xl md:text-5xl md:mb-20 font-bold mb-4">Login</h1>
+        <div className="flex flex-col md:w-1/3 w-5/6 p-5 rounded-xl shadow-lg bg-profile-content">
+          <form onSubmit={handleSubmit}>
+            <ul className="mb-6 space-y-4">
+              <li className=" font-bold">
+                Email adress:
                 <input
                   type="email"
                   id="email"
@@ -68,13 +73,13 @@ const LoginUser = () => {
                   className="w-full p-2 text-gray-700 placeholder:text-black placeholder-opacity-75 focus:outline-none"
                   placeholder="Email"
                   onChange={handleInput}
-                /> 
+                />
                 {errors.email && (
                   <span className="text-danger"> {errors.email} </span>
                 )}
-                </li>
-                <li className=" font-bold">
-                  Password:
+              </li>
+              <li className=" font-bold">
+                Password:
                 <input
                   type="password"
                   id="password"
@@ -87,8 +92,8 @@ const LoginUser = () => {
                   <span className="text-danger"> {errors.password} </span>
                 )}
               </li>
-              </ul>
-              <div className="flex justify-end">
+            </ul>
+            <div className="flex justify-end">
               <button
                 type="submit"
                 className="px-4 py-1 text-white rounded-md hover:text-aubergine bg-btn-gradient">
@@ -96,10 +101,9 @@ const LoginUser = () => {
               </button>
             </div>
           </form>
-          <h2
-          onClick={() => navigateHome()}
-          >Not registred yet? Click here!</h2>
+          <h2>Not registred yet? Click here!</h2>
         </div>
+        <div>{loginError}</div>
       </div>
     </>
   );
--- frontend/src/pages/login/loginValidation.tsx
+++ frontend/src/pages/login/loginValidation.tsx
@@ -2,8 +2,8 @@
 
 const Validation = (values: any) => {
   let errors: any = {};
-  const email_pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z].{2,4}$/;
-   const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,20}$/;
+  const email_pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+   const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/;
   if (values.email === '') {
     errors.email = 'Email is required';
   } else if (!email_pattern.test(values.email)) {
@@ -13,7 +13,7 @@ const Validation = (values: any) => {
   }
   if (values.password === '') {
     errors.password = 'Password is required';
-  } else if(password_pattern.test(values.password))
+  } else if(!password_pattern.test(values.password))
         {
             errors.password = "Password is invalid";
         } else {
