de529a8d7d3f7895c96529014921f6e535a6ad98
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-06-03 14:32:15 +0100

    icon api changes

---
+++ frontend/src/utils/apiExport.tsx
@@ -0,0 +1,13 @@
+import getIconApi from '../components/reusable_collapsable/iconApi';
+
+export const manaCostRe = (manaCost: string) => {
+  return getIconApi(manaCost).map((url: string, index: number) => (
+    <div key={index} className='h-4 w-4 text-nowrap flex flex-row'>
+      <img
+        src={url}
+        alt={`mana Symbol ${index}`}
+        className='text-nowrap'
+      />
+    </div>
+  ));
+};
