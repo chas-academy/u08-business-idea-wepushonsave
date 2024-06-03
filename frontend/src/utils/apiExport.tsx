c97c5ce880928c63bb0326d86ddc64495c20dccd
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-06-03 14:34:12 +0100

    eslint

--- frontend/src/utils/apiExport.tsx
+++ frontend/src/utils/apiExport.tsx
@@ -1,13 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+
 import getIconApi from '../components/reusable_collapsable/iconApi';
 
 export const manaCostRe = (manaCost: string) => {
   return getIconApi(manaCost).map((url: string, index: number) => (
-    <div key={index} className='h-4 w-4 text-nowrap flex flex-row'>
-      <img
-        src={url}
-        alt={`mana Symbol ${index}`}
-        className='text-nowrap'
-      />
+    <div key={index} className="h-4 w-4 text-nowrap flex flex-row">
+      <img src={url} alt={`mana Symbol ${index}`} className="text-nowrap" />
     </div>
   ));
 };
